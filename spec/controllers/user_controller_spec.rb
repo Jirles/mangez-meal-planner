3e9cfc167bf5a05aa41820416be72bf5f93ff472
Jirles <emily.jirles@gmail.com>
2018-06-04 12:35:30 -0400

    Build signup page and routing, begin building out tests

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -9,5 +9,31 @@ describe 'UserController' do
       expect(page.body).to include("</form>")
     end
 
+    it 'has a form with fields for username, email, and password' do
+      visit '/signup'
+
+      fill_in(:username, :with => "testqueen")
+      fill_in(:email, :with => "all_hail@test.com")
+      fill_in(:password, :with => "testytest")
+    end
+
+    it 'does not send a form with a blank field' do
+      fill_in(:username, :with => "testqueen")
+      fill_in(:email, :with => "all_hail@test.com")
+      click_button 'submit'
+
+      expect(last_response.location).to include("/signup")
+    end
+
+    it 'sends a new user to their profile' do
+      params = {
+         :username => "skittles123",
+         :email => "skittles@aol.com",
+         :password => "rainbows"
+       }
+       post '/signup', params
+       expect(last_response.location).to include("/profile/")
+     end
+
   end
 end
