31c269a42e0e6f1db5bb44ecdb192542be91d61c
Jirles <emily.jirles@gmail.com>
2018-06-04 18:03:45 -0400

    Wrote specs for index page to check it displays recipes and only logged in users can see it

--- spec/controllers/recipe_controller_spec.rb
+++ spec/controllers/recipe_controller_spec.rb
@@ -1,23 +1,29 @@
 require 'spec_helper'
 
 describe "Recipe Controller" do
-  context "recipes index page" do
+  context "recipe index page" do
     before do
-      @user = User.create(:username => "testking", :email => "long_live_the_king@test.com", :password => "testingtesting")
+      @user = User.create(username: "test queen", email: "all_hail@test.com", password: "supersecret")
       Recipe.create(name: "Mac 'n' Cheese", ingredients: "cheese, macaroni, milk, butter", instruction: "mix it together in a pot", user_id: @user.id)
       Recipe.create(name: "Cobb Salad", ingredients: "lettuce greens, eggs, chicken, dressing of choice", instruction: "mix it together in a bowl", user_id: @user.id)
-      params = {:username => "testking", :email => "long_live_the_king@test.com", :password => "testingtesting"}
-      post '/signup', params
     end
 
-    it "displays all recipes to the user" do
-      visit '/recipes'
-      expect(page.status_code).to eq(200)
-      expect(page.body).to include("Welcome, ")
-      expect(page.body).to include("Mac 'n' Cheese")
+    it "users can only visit the index page if they are already logged in" do
+      get '/logout'
+
+      get '/recipes'
+      expect(last_response.location).to include("/login")
     end
 
+    it "displays recipes to the user" do
+      params = {username: "test queen", password: "supersecret"}
+      post '/login', params
+
+      expect(last_response.status).to eq(302)
+      follow_redirect!
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include("Mac 'n' Cheese")
+    end
   end
-
 
 end
