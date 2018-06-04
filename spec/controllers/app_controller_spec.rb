0950d8542188528c6b1a130c50fef8f17f2b087c
Jirles <emily.jirles@gmail.com>
2018-06-04 11:23:42 -0400

    Small changes to AppController

--- spec/controllers/app_controller_spec.rb
+++ spec/controllers/app_controller_spec.rb
@@ -1,18 +1,16 @@
 require 'spec_helper'
 
 describe "AppController" do
-  it "handles traffic leading to homepage" do
-    before do
-      visit "/"
-    end
+  before do
+    visit "/"
+  end
 
-    it 'responds with a 200 status code' do
-      expect(page.status_code).to eq(200)
-    end
+  it 'responds with a 200 status code' do
+    expect(page.status_code).to eq(200)
+  end
 
-    it "welcomes the user" do
-      expect(page.body).to include("Welcome to Mangez")
-    end
+  it "welcomes the user" do
+    expect(page.body).to include("Welcome to Mangez")
   end 
 
 end
