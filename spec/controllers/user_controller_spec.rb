e81b916506950d7170cdfe0cfb5ea58038f20557
Jirles <emily.jirles@gmail.com>
2018-06-04 14:01:45 -0400

    Create login/logout features, tests passed

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -42,29 +42,39 @@ describe 'UserController' do
 
   context 'login page' do
     before do
-      visit '/login'
+      User.create(:username => "testking", :email => "long_live_the_king@test.com", :password => "testingtesting")
     end
 
     it 'when a user is directed to the login page they see a form' do
+      visit '/login'
       expect(page.status_code).to eq(200)
       expect(page.body).to include("</form>")
     end
 
-    it 'has a form with fields for username, and password' do
-      fill_in(:username, :with => "testqueen")
-      fill_in(:email, :with => "all_hail@test.com")
-      fill_in(:password, :with => "testytest")
+    it 'has a form with fields for username and password' do
+      visit '/login'
+      fill_in(:username, :with => "testking")
+      fill_in(:password, :with => "testingtesting")
+    end
+
+    it 'redirects a user to the recipes index after log in' do
+      params = {:username => "testking", :password => "testingtesting"}
+      post '/login', params
+
+      visit '/recipes'
+      expect(last_response.location).to include("/recipes")
     end
   end
 
   context 'logout action' do
-    it 'logs a person out of their session by clearing the session[:user_id]' do
-      visit '/logout'
+    it 'logs a person out of the app' do
+      params = { :username => "testking", :password => "testingtesting"}
+      post '/login', params
 
-      visit '/recipes'
-      expect(last_response.location).to include(/login)
+      get '/logout'
+      get '/recipes' #=> set up to redirect to login if no user is currently logged in
+      expect(last_response.location).to include('/login')
     end
-
   end
 
 end
