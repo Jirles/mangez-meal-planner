4f855099b1431d67734ff25b2f719b85d2b222f9
Jirles <emily.jirles@gmail.com>
2018-06-04 11:20:50 -0400

    Set up basic homepage, layout and set up AppController

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,3 +1,28 @@
 class AppController < Sinatra::Base
+  configure do
+    set :views, 'app/views'
+    set :public_folder, 'public'
+    enable :sessions
+    set :session_secret, 'bonappetit!'
+  end
+
+  get '/' do
+    erb :home
+  end
+
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def redirect_if_not_logged_in
+      redirect '/login' if !logged_in?
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 
 end
---
+++ spec/controllers/app_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "AppController" do
+  it "handles traffic leading to homepage" do
+    before do
+      visit "/"
+    end
+
+    it 'responds with a 200 status code' do
+      expect(page.status_code).to eq(200)
+    end
+
+    it "welcomes the user" do
+      expect(page.body).to include("Welcome to Mangez")
+    end
+  end 
+
+end
