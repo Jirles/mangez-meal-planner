7638ec057e6c7646e835bfcc2342786ced4cd9c0
Jirles <emily.jirles@gmail.com>
2018-06-05 16:06:29 -0400

    Add first tests for show meal plan page and routing, tests will currently fail

--- app/controllers/meal_plan_controller.rb
+++ app/controllers/meal_plan_controller.rb
@@ -7,6 +7,13 @@ class MealPlanController < AppController
     erb :'meal_plans/create_mp'
   end
 
+  get '/meal-plans/:id' do
+    @meal_plan = MealPlan.find(params[:id])
+    access_check(@meal_plan)
+
+    erb :'/meal_plans/show_mp'
+  end
+
   post '/meal-plans' do
     params[:user_id] = current_user.id
     MealPlan.create(params)
--- spec/controllers/meal_plan_spec.rb
+++ spec/controllers/meal_plan_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper'
 
 describe "Meal Plan Controller" do
-    context "create meal plan page" do
-    before do
-      @user = User.create(username: "test queen", email: "all_hail@test.com", password: "supersecret")
-      @mac_n_cheese = Recipe.create(name: "Mac 'n' Cheese", ingredients: "cheese, macaroni, milk, butter", instruction: "mix it together in a pot", user_id: @user.id)
-      @cobb_salad = Recipe.create(name: "Cobb Salad", ingredients: "lettuce greens, eggs, chicken, dressing of choice", instruction: "mix it together in a bowl", user_id: @user.id)
-      @cereal = Recipe.create(name:"Fruity Pebbles", ingredients: "fruity pebbles, milk", instruction:"pour into a bowl", user_id: @user.id)
-      params = {username: "test queen", password: "supersecret"}
-      post '/login', params
-    end
+  before do
+    @user = User.create(username: "test queen", email: "all_hail@test.com", password: "supersecret")
+    @mac_n_cheese = Recipe.create(name: "Mac 'n' Cheese", ingredients: "cheese, macaroni, milk, butter", instruction: "mix it together in a pot", user_id: @user.id)
+    @cobb_salad = Recipe.create(name: "Cobb Salad", ingredients: "lettuce greens, eggs, chicken, dressing of choice", instruction: "mix it together in a bowl", user_id: @user.id)
+    @cereal = Recipe.create(name:"Fruity Pebbles", ingredients: "fruity pebbles, milk", instruction:"pour into a bowl", user_id: @user.id)
+    params = {username: "test queen", password: "supersecret"}
+    post '/login', params
+  end
 
+  context "create meal plan page" do
     it "displays a form to the user to create a meal plan" do
       get '/meal-plans/new'
       expect(last_response.status).to eq(200)
@@ -50,7 +50,7 @@ describe "Meal Plan Controller" do
       expect(last_response.location).to include("/login")
     end
 
-    it "saves a meal plan with the defaul setting if a name is not supplied" do
+    it "saves a meal plan with the default name if a name is not supplied" do
       params = {
         :breakfast => @cereal.id,
         :lunch => @mac_n_cheese.id,
@@ -65,6 +65,47 @@ describe "Meal Plan Controller" do
       expect(last_response.body).to include("Your Meal Plan")
       expect(MealPlan.last.name).to eq("Your Meal Plan")
     end
+  end
 
+  context "view meal plan page" do
+    before do
+      @noice_mp = MealPlan.create({
+        :name => "Noice Meal Plan",
+        :breakfast => @cereal.id,
+        :lunch => @mac_n_cheese.id,
+        :dinner => @cobb_salad.id,
+        :user_id => @user.id
+      })
+    end
+
+    it "shows an individual recipe and its details" do
+      get "/meal-plans/#{@noice_mp.id}"
+
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include("Noice Meal Plan")
+      expect(last_response.body).to have_link("Fruity Pebbles")
+    end
+
+    it "does not let a user visit unless they are logged in" do
+      get '/logout'
+
+      get "/recipes/#{@noice_mp.id}"
+      expect(last_response.status).to eq(302)
+      follow_redirect!
+      expect(last_response.body).to include("Welcome back")
+    end
+
+    it "does not let a user visit unless they have owner permissions" do
+      get '/logout'
+      User.create(:username => "testking", :email => "long_live_the_king@test.com", :password => "testingtesting")
+      params = {:username => "testking", :password => "testingtesting"}
+      post '/login', params
+
+      get "/recipes/#{@noice_mp.id}"
+      expect(last_response.status).to eq(302)
+      follow_redirect!
+      expect(last_response.body).to include("Welcome, testking")
+      expect(last_response.location).to include("/profile/testking")
+    end
   end
 end
